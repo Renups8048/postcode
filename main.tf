a35e81dc66540a1e31cc47018f2eeef88706f77f
Renuka Prasad <renukaprasads8048@gmail.com>
2023-03-06 12:25:35 +0000

    feat:create ECS with SG and VPC

--- main.tf
+++ main.tf
@@ -1,31 +1,99 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
+# Provider configuration
+provider "aws" {
+  region = "eu-west-1"
 }
 
+# Variables
+variable "app_name" {
+  default = "terra-ecs-app"
+}
 
-#Terraform sample to create EC2 instance
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+# Create a VPC for the ECS cluster
+resource "aws_vpc" "ecs_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create an Internet Gateway for the VPC
+resource "aws_internet_gateway" "ecs_igw" {
+  vpc_id = aws_vpc.ecs_vpc.id
+}
+
+# Create a public subnet for the ECS cluster
+resource "aws_subnet" "ecs_subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.ecs_vpc.id
 
   tags = {
-    Name = "HelloWorld"
+    Name = "${var.app_name}-public-subnet"
   }
 }
 
-#Terraform sample to create ECR instance
-resource "aws_ecr_repository" "example" {
-  name = "terra-repo" # replace with your desired repository name
+# Create a security group for the ECS instances
+resource "aws_security_group" "ecs_sg" {
+  name_prefix = "${var.app_name}-"
+  vpc_id      = aws_vpc.ecs_vpc.id
+
+  ingress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+# Create an ECS cluster
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "${var.app_name}-cluster"
+}
+
+# Create an EC2 launch configuration
+resource "aws_launch_configuration" "ecs_lc" {
+  name_prefix                 = "${var.app_name}-"
+  instance_type               = "t2.micro"
+  iam_instance_profile        = "ecsInstanceRole"
+  security_groups             = [aws_security_group.ecs_sg.id]
+  associate_public_ip_address = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create an Auto Scaling group for the ECS instances
+resource "aws_autoscaling_group" "ecs_asg" {
+  name                      = "${var.app_name}-asg"
+  launch_configuration      = aws_launch_configuration.ecs_lc.id
+  min_size                  = 1
+  max_size                  = 3
+  desired_capacity          = 1
+  vpc_zone_identifier       = [aws_subnet.ecs_subnet.id]
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+
+  tag {
+    key                 = "Name"
+    value               = "${var.app_name}-ecs-instance"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create an ECR repository
+resource "aws_ecr_repository" "ecs_repo" {
+  name = "${var.app_name}-repo"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
